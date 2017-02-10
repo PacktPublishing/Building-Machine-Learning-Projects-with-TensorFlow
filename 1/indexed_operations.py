d30c24e6deb9483ade3c6e4728f2fde4c2549484
Naveen Kumar <nav007@gmail.com>
2017-02-11 00:24:06 +0530

    listdiff renamed to setdiff1d to reflect API change

--- 1/indexed_operations.py
+++ 1/indexed_operations.py
@@ -11,6 +11,6 @@ boolx = tf.constant([[True,False], [False,True]])
 
 tf.argmin(x, 1).eval() # Position of the maximum value of columns
 tf.argmax(x, 1).eval() # Position of the minimum value of rows
-tf.listdiff(listx, listy)[0].eval() # List differences
+tf.setdiff1d(listx, listy)[0].eval() # List differences
 tf.where(boolx).eval() # Show true values
 tf.unique(listx)[0].eval() # Unique values in list
